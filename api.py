01b1edd1dccce5809c08af00676486be0cebf475
aure <mouretaurelien95git@gmail.com>
2018-06-13 10:35:32 +0200

    pokemeon class a&dded

--- api.py
+++ api.py
@@ -4,14 +4,44 @@ import hug
 
 from bs4 import BeautifulSoup
 
-with open("pokemon.html") as file:
-    soup = BeautifulSoup(file,"lxml")
-    soup.find(id="pokedex")
+#////////////////////////////////////////////////////////////////////
+class Pokemon:
+        name=""
+        type=""
+        total=""
+        attack=""
+        defense=""
+        sp_attack=""
+        sp_defense=""
+        speed=""
+        speed=""
+'''
+#pass= permet de redefinir constructeur
+def __init__(self):
 
-    for tab in  soup.find_all("tr"):
-        for data in tab.find_all("td"):
-            print(data.text)
-            print("\n")
+    pass
+'''
+def __init__(self,name,type,total,attack,defense,sp_attack,sp_defense,speed ):
+
+    self.name=name
+    self.type=type
+    self.total=total
+    self.attack=attack
+    self.defense=defense
+    self.sp_attack=sp_attack
+    self.sp_defense=sp_defense
+    self.speed=speed
+    pass
+
+#/////////////////////////////////////////////////////////////
+    with open("pokemon.html") as file:
+        soup = BeautifulSoup(file,"lxml")
+        soup.find(id="pokedex")
+
+        for tab in  soup.find_all("tr"):
+         for data in tab.find_all("td"):
+             print(data.text)
+             print("\n")
 
 
 
