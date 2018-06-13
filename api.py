95548dbde0869c4554a2db6109d7fe8a9b2e612f
aure <mouretaurelien95git@gmail.com>
2018-06-13 11:06:39 +0200

    class pokemon ok

--- api.py
+++ api.py
@@ -6,20 +6,46 @@ from bs4 import BeautifulSoup
 
 #////////////////////////////////////////////////////////////////////
 class Pokemon:
-        name=""
-        type=""
-        total=""
-        attack=""
-        defense=""
-        sp_attack=""
-        sp_defense=""
-        speed=""
-        speed=""
-'''
-#pass= permet de redefinir constructeur
-def __init__(self):
+
+    '''
+    #pass= permet de redefinir constructeur
+    def __init__(self):
 
     pass
+    '''
+
+
+
+    def __init__(self):
+
+        self.name = "ame"
+        self.type = "type"
+        self.total = "total"
+        self.attack = "attack"
+        self.defense = "defense"
+        self.sp_attack = "sp_attack"
+        self.sp_defense = "sp_defense"
+        self.speed = "speed"
+
+        pass
+
+    def getData(self):
+        data = []
+
+        data.append(self.name)
+        data.append(self.type)
+        data.append(self.total)
+        data.append(self.attack)
+        data.append(self.defense)
+        data.append(self.sp_attack)
+        data.append(self.sp_defense)
+        data.append(self.speed)
+
+
+
+
+
+
 '''
 def __init__(self,name,type,total,attack,defense,sp_attack,sp_defense,speed ):
 
@@ -32,31 +58,27 @@ def __init__(self,name,type,total,attack,defense,sp_attack,sp_defense,speed ):
     self.sp_defense=sp_defense
     self.speed=speed
     pass
+'''
+
+
 
 #/////////////////////////////////////////////////////////////
-    with open("pokemon.html") as file:
-        soup = BeautifulSoup(file,"lxml")
-        soup.find(id="pokedex")
 
-        for tab in  soup.find_all("tr"):
-         for data in tab.find_all("td"):
+
+    #pokemon=Pokemon()
+
+    #print(pokemon.getData())
+
+with open("pokemon.html") as file:
+    soup = BeautifulSoup(file,"lxml")
+    soup.find(id="pokedex")
+
+    for tab in  soup.find_all("tr"):
+        for data in tab.find_all("td"):
              print(data.text)
              print("\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print("coucou ")
+pokemon=Pokemon()
+print("hola")
+print(pokemon.name)
+print("como esta")
